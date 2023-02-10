322591738641fd8ef87b9e822dadfeb82e7249d3
AnaBib <a.bibilashvili@smartsoft.ge>
2023-02-10 17:58:57 +0400

    add message

--- first.js
+++ first.js
@@ -1,3 +1,17 @@
+const LOCALE =[
+
+  { 'task.error.message':'ველი ცარიელია!',
+     'task.success.message':'წარმატებით დაემატა'
+
+
+}
+
+]
+
+
+
+
+
 let array = [
     {
       id: '123gt45bcvrtt00',
@@ -13,10 +27,10 @@ let array = [
 
 ];
 
-let array1 = [1,2,3,4,5,6,7]
+
 
 let html = '';
-for(let index = 0; index<array1.length; index++)
+for(let index = 0; index<array.length; index++)
 
  {
 
@@ -39,21 +53,90 @@ for(let index = 0; index<array1.length; index++)
 
  `;
 
-
-
+ }
 
  document.getElementById('Taskbox').innerHTML = html;
-  function addList()
-  {
-    let inputvalue = document.getElementById('inputValue');
-    console.log(inputvalue)
 
-  }
+
+ function addList(){
 
   
- 
+  let message = document.getElementById('errorMessage')
+  let inputvalue = document.getElementById('inputV').value;
+
+  if(inputvalue.length===0){
+   message.classList.add('error')
+   message.innerText = getCaption('task.error.message');
+    setTimeout(function()  {
+      message.classList.remove('error')
+    },2000)
+   
+    
+  }
+
+  else{
+  
+  
+array.push(
+  {
+    name:inputvalue,
+    id:124564455,
+    completed:false
+
+
+    
+  })
+  message.classList.add('success')
+  setTimeout(function()  {
+    message.classList.remove('success')
+    
+  },2000)
+  message.innerText = getCaption('task.success.message');
+  drawTask();
+
+     }
+
+
+    }
+   
+
+function drawTask(){
+  
+  let html = '';
+  for(let index = 0; index<array.length; index++)
+  
+   {
+  
+    html += `
+    <div class="boxmodel2 ${array[index].completed ? 'task' : ' '}">
+         <h3> 
+        <span class="done" ></span>
+           ${array[index].name}
+        </h3>
+        <div class="buttons">
+           <a class="edit" href="edit_task.html"></a>
+           <button class="delete_button"></button>
+           
+           </div>
+          
+   </div>
+  
+  
+   
+  
+   `;
+  
+   }
+   document.getElementById('Taskbox').innerHTML = html;
+   }
+
+
+
+  drawTask();
+
+
+function getCaption(key){
+return LOCALE[key]
+
+
 }
-
-
-
-
