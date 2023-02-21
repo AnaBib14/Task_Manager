9c6a0b6c7cffd2dd3f56579a89f14250ab0b12d5
AnaBib14 <123357888+AnaBib14@users.noreply.github.com>
2023-02-21 17:05:57 +0400

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,5 @@
+let containerSlider = document.querySelector(".slider-container")
+let buttons = document.querySelectorAll("button")
+let allImgs = document.querySelectorAll("img").length
+
+let curentIndex = 1;
