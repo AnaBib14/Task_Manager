fbb4bf60ee3b8b9b874b15dec68cf2b0e853787c
AnaBib14 <123357888+AnaBib14@users.noreply.github.com>
2023-02-21 17:07:30 +0400

    Delete slider.js

--- slider.js
+++
@@ -1,5 +0,0 @@
-let containerSlider = document.querySelector(".slider-container")
-let buttons = document.querySelectorAll("button")
-let allImgs = document.querySelectorAll("img").length
-
-let curentIndex = 1;
